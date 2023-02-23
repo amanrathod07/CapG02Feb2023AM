dd939fabcd2e042b9043e9584f77298c5619c338
amanrathod07 <amanrathod07.ar@gmail.com>
2023-02-23 22:21:44 +0530

    updated

---
+++ day4/prog3.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+int main(){
+
+char c;
+
+printf("\n input a character:");
+
+c=getchar();
+
+printf("\nthe input value=%c\tAscii value=%d\n",c,c);
+
+printf("\n\n");
+
+return 0;
+}
---
+++ day4/prog4.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+
+int main(){
+
+char str[100]="This is to test string";
+//char *str1;
+
+printf("\n%s",str);
+printf("\n\n");
+
+return 0;
+}
